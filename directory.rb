1c97f27b5bad46ac18b86b83be4b2ab470c4eb1a
Sumner1185 <alastair.sumner@yahoo.com>
2020-05-04 17:29:22 +0100

    Add input_students method

--- directory.rb
+++ directory.rb
@@ -1,19 +1,13 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
   students = []
-  # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty?
-    # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-    # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
   students
 end
 
@@ -23,7 +17,7 @@ def print_header
 end
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]}" + "(#{student[:cohort]} cohort)".center(35)
   end
 end
 def print_footer(students)
