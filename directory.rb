0a5b688a2f62fbb00f09cdf0de064bb2b8500f32
Sumner1185 <alastair.sumner@yahoo.com>
2020-05-05 09:30:11 +0100

    Add singular puts for first student

--- directory.rb
+++ directory.rb
@@ -1,17 +1,23 @@
 def input_students
+  cohorts = [:January, :February, :March, :April, :May, :June, 
+    :July, :August, :September, :October, :November, :December]
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
   while !name.empty?
     puts "Please add the students cohort"
-    cohort = gets.chomp.capitlaize.to_sym
+    cohort = gets.capitalize.chomp.to_sym
     unless cohorts.include?(cohort)
       puts "Type error: please enter student and cohort again"
     else
       students << {name: name, cohort: cohort}
     end
-    puts "Now we have #{students.count} students"
+    if students.length == 1 
+      puts "Now we have #{students.count} student"
+    elsif students.length > 1
+      puts "Now we have #{students.count} students"
+    end
     name = gets.chomp
   end
   students
