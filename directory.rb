040189402a7e3e7c66c5e642052b2fdb94f7111b
Sumner1185 <alastair.sumner@yahoo.com>
2020-05-05 15:24:03 +0100

    Refactor

--- directory.rb
+++ directory.rb
@@ -1,22 +1,20 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.capitalize.chomp
   while !name.empty?
     cohort = input_cohort
-    students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
 def input_cohort
   cohorts = [:January, :February, :March, :April, :May, :June, 
   :July, :August, :September, :October, :November, :December]
   puts "Please add the students cohort"
-    cohort = gets.capitalize.chomp.to_sym
-    unless cohorts.include?(cohort)
+    input = gets.capitalize.chomp.to_sym
+    unless cohorts.include?(input)
         puts "Type error"
         cohort = input_cohort
     end
@@ -26,9 +24,9 @@ def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-def print(students)
-  if students.length >= 1
-    students.each_with_index do |student, index|
+def print
+  if @students.length >= 1
+    @students.each_with_index do |student, index|
       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   else
@@ -36,11 +34,41 @@ def print(students)
     input_students
   end
 end
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+def interactive_menu
+  loop do
+    menu = print_menu
+    selection = gets.chomp
+    process(selection)
+  end
+end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+def show_students
+  print_header
+  print
+  print_footer
+end
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show = show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
-students = input_students
+@students = []
+menu = interactive_menu
 print_header
-print(students)
-print_footer(students)
+print
+print_footer
